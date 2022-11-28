20d35844a8de42e17711bbb11e6744d7dc057693
Pominov7 <Pominov7@mail.ru>
2022-11-29 00:33:36 +0700

    add DBUserDetails

--- src/main/java/org/top/studentsapplication/service/security/DBUserDetails.java
+++ src/main/java/org/top/studentsapplication/service/security/DBUserDetails.java
@@ -1,43 +1,56 @@
 package org.top.studentsapplication.service.security;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.top.studentsapplication.db.entity.User;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class DBUserDetails implements UserDetails {
+
+    // оборачиваем обычного пользователя
+    private User dbUser;
+
+    public DBUserDetails(User dbUser) {
+        this.dbUser = dbUser;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.<GrantedAuthority>singletonList(
+                new SimpleGrantedAuthority("USER")
+        );
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return dbUser.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return dbUser.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }

1904f3185659c6f5c6ea5f330171c7ed7d0c90fb
Pominov7 <Pominov7@mail.ru>
2022-11-29 19:02:01 +0700

    project fine-tuning

--- src/main/java/org/top/studentsapplication/config/WebSecurityConfig.java
+++ src/main/java/org/top/studentsapplication/config/WebSecurityConfig.java
@@ -1,6 +1,10 @@
 package org.top.studentsapplication.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -10,7 +14,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.top.studentsapplication.service.security.DBUserDetailsService;
+
+import javax.sql.DataSource;
 
 
 @EnableWebSecurity
@@ -20,6 +29,10 @@ public class WebSecurityConfig {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+    @Autowired
+    public DBUserDetailsService dbUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,23 +52,23 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails root =
-                User.withDefaultPasswordEncoder()
-                        .username("root")
-                        .password("root")
-                        .roles("ADMIN")
-                        .build();
-
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("user")
-                        .roles("USER")
-                        .build();
-        return new InMemoryUserDetailsManager(root, user);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails root =
+//                User.withDefaultPasswordEncoder()
+//                        .username("root")
+//                        .password("root")
+//                        .roles("ADMIN")
+//                        .build();
+//
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("user")
+//                        .roles("USER")
+//                        .build();
+//        return new InMemoryUserDetailsManager(root, user);
+//    }
 
     @Bean
     public WebSecurityCustomizer ignoringCustomizer() {
--- src/main/java/org/top/studentsapplication/service/security/DBUserDetailsService.java
+++ src/main/java/org/top/studentsapplication/service/security/DBUserDetailsService.java
@@ -6,18 +6,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.top.studentsapplication.db.entity.User;
+import org.top.studentsapplication.db.repository.UserRepository;
 import org.top.studentsapplication.service.UserService;
 
 @Service
 public class DBUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         // 1. получить пользователя
-        User user = userService.getUserByUserName(userName);
+        User user = userRepository.findByUsername(userName);
+        if (user == null) {
+            // если пользователь не найден
+            throw new UsernameNotFoundException(userName);
+        }
         // 2. вернуть объект UserDetails
         return new DBUserDetails(user);
     }
